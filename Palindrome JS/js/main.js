095db0c4c4ff7f2af368831c39deb2f82bfd826e
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-12 17:19:55 +0100

    Edit Form in JS

--- Palindrome JS/js/main.js
+++ Palindrome JS/js/main.js
@@ -13,22 +13,33 @@ console.log('myButton', myButton, typeof myButton);
 myButton.addEventListener('click', function() {
 
     console.log(myButton);
-    // Dichiaro una Variabile in cui racchiudo l'input dell'utente
-    const userWord = document.getElementById('user-word').value;
-    console.log('userWord', userWord, typeof userWord);
+
+    // Dichiaro una Variabile in cui racchiudo il Form
+    const form = document.querySelector('form');
+
+    // Creo l'evento per la sottomissione del Form
+    form.addEventListener('click', function(event) {
+
+        // Dichiaro una Variabile in cui racchiudo l'input dell'utente
+        const userWord = document.getElementById('user-word').value;
+        console.log('userWord', userWord, typeof userWord);
 
 
-    // Inizializzo una variabile con all'interno la funzione che abbia come argomento l'input dell'utente
-    const userReverse = inputReverse(userWord);
-    console.log(userReverse);
+        // Inizializzo una variabile con all'interno la funzione che abbia come argomento l'input dell'utente
+        const userReverse = inputReverse(userWord);
+        console.log(userReverse);
 
-    if (userReverse == userWord) {
-        alert ('Bravo')
-    } else {
-        alert ('Hai sbagliato');
-    }
+        if (userReverse == userWord) {
+            alert ('Bravo')
+        } else {
+            alert ('Hai sbagliato');
+        }
 
-})
+    });
+
+
+
+});
 /*
 
     2) Creare una funzione per capire se la parola inserita è palindroma.
